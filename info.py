26ba5a1350d9ca7b658f4c85b80a5f8c8fa2e461
skalight <119775283+skalight@users.noreply.github.com>
2023-04-30 13:26:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,14 +22,14 @@ BOT_TOKEN = environ.get('BOT_TOKEN', 'BotToken')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/cb0cf8d856e66a8991970.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/bab8959162b9656abfed9.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/cb0cf8d856e66a8991970.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/fadf76229a7c7de7a7cff.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5296610774').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001660257089').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5296610774 1491400016').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001782441380').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
@@ -51,24 +51,24 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'rxmov_files')
 
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tnlink.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'b6aace46d40c605fff8e0cafbcd8fbe416851f4d')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '').split()]
 PORT = environ.get("PORT", "8080")
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+ZPpcbtCV204yYWU1')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/RolexMoviesOXO')
-MOVIE_GRP = environ.get('MOVIE_GRP', 'https://t.me/+ZPpcbtCV204yYWU1')
-SERIES_GRP = environ.get('SERIES_GRP', 'https://t.me/RolexMoviesOXO')
-MAIN_CHNL = environ.get('MAIN_CHNL', 'https://telegram.me/TVSeriesCW')
-MSG_ALRT = environ.get('MSG_ALRT', '🚩 @RolexMoviesOXO Best Channel In Telegram')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001660274107'))
-REQUEST_LOGS = int(environ.get('REQUEST_LOGS', '-1001877931898'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'RolexMoviesOXO')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/jailmoviehdbackup_08')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Jail_Linkzz')
+MOVIE_GRP = environ.get('MOVIE_GRP', 'https://t.me/jailmoviehdbackup_08')
+SERIES_GRP = environ.get('SERIES_GRP', 'https://t.me/Jail_Linkzz')
+MAIN_CHNL = environ.get('MAIN_CHNL', 'https://t.me/Jail_Linkzz')
+MSG_ALRT = environ.get('MSG_ALRT', '🚩 @Jail_Linkzz Best Channel In Telegram')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001654008278'))
+REQUEST_LOGS = int(environ.get('REQUEST_LOGS', '-1001654008278'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Jail_Linkzz')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
-AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
+AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "False")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
 BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
@@ -94,5 +94,5 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
      # Auto Delete For Group Message (Self Delete) #
 SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 10800))
-DOWNLOAD_TEXT_URL = "https://t.me/tnlinkdown/6"
-CAPTION_BUTTON_URL = "https://t.me/RolexMoviesOXO"
+DOWNLOAD_TEXT_URL = "https://t.me/Jail_Linkzz"
+CAPTION_BUTTON_URL = "https://t.me/Jail_Linkzz"
